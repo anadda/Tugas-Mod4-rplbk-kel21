acf8f623964ef487388295a03c28434e97c9125e
anadda <63113056+anadda@users.noreply.github.com>
2022-10-30 14:34:23 +0700

    Input text with useState

--- src/learn-useState/Counter.js
+++ src/learn-useState/Counter.js
@@ -3,42 +3,20 @@ import logo from '../logo.svg';
 import './Counter.css';
 
 export default function Counter() {
-  const [showImage, setShowImage] = useState(true);
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount(count + 1);
-  };
-  const countDown = () => {
-    setCount(count - 1);
-  };
-  const hideImage = () => {
-    setShowImage(!showImage);
-  };
+  const [text, setText] = React.useState('');
+  const [showImage] = useState(true);
+
   return (
     <div className="Main">
-      <p className="Text">Counter with useState</p>
+      <p className="Text">Input Type with useState</p>
       <p>KELOMPOK 21</p>
       <div className="ViewImage">
         <div className="ViewImage1">
           <img className="App-logo" style={{ display: showImage === true ? 'flex' : 'none' }} src={logo} alt="logo" />
         </div>
-        <button className="Button" onClick={hideImage}>
-          {showImage !== true ? 'show' : 'hide'}
-        </button>
       </div>
-      <p className="Text">{count}</p>
-      <div className="ViewButton">
-        <div className="ViewButton2">
-          <button className="Button" onClick={countUp}>
-            Up
-          </button>
-        </div>
-        <div className="ViewButton1">
-          <button className="Button" onClick={countDown}>
-            Down
-          </button>
-        </div>
-      </div>
+      <input type="text" placeholder="" onChange={(e) => setText(e.target.value)} />
+      <p>{text}</p>
     </div>
   );
 }
